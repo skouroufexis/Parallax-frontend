0dd47ce2e0d6407312da1a6e7277b0958f736828
skouroufexis <skouroufexis@gmail.com>
2021-04-20 20:35:41 +0300

    added "products" section

---
+++ index.js
@@ -0,0 +1,13 @@
+let productContainers=document.getElementsByClassName('product-container');
+let l=productContainers.length;
+let c;
+
+for (c=0;c<l;c++){
+    if(c!=0){
+        let path='./images/products'+c+'.png';
+        productContainers[c].style.backgroundImage='url('+path+')';
+
+    }
+}
+
+
