df8416281b385070fbad2ee5a318b5d303af2a5a
skouroufexis <skouroufexis@gmail.com>
2021-04-21 12:04:46 +0300

    added mobile menu

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 let productContainers=document.getElementsByClassName('product-container');
 let l=productContainers.length;
 let c;
@@ -10,6 +11,25 @@ for (c=0;c<l;c++){
     }
 }
 
+function openMenu(){
 
+    document.getElementById('mobileMenuButton').style.display='none';
+    let menuContainer=document.getElementById('mobileMenuLinksContainer');
+    menuContainer.style.display='block';
+    setTimeout(() => {
+        menuContainer.style.transform='translateX(0)';  
+        menuContainer.style.opacity='1';    
+    }, 200);
+    
+}
 
-
+function closeMenu(){
+    let menuContainer=document.getElementById('mobileMenuLinksContainer');
+    menuContainer.style.transform='translateX(-20px)';  
+    menuContainer.style.opacity='0';  
+    
+    setTimeout(() => {
+        menuContainer.style.display='none';
+        document.getElementById('mobileMenuButton').style.display='block';
+    }, 200);
+}
